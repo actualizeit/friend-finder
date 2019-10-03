4ffd709538b28dc7437a597fddfe7ae482a8b829
dbell <greenadvisor@gmail.com>
2019-10-03 11:34:50 -0400

    all functionality working locally

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,7 +5,7 @@
 var friends = [
     {
       name: "Ahmed",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      photo: "https://via.placeholder.com/150",
       "scores":[
         5,
         1,
@@ -20,44 +20,5 @@ var friends = [
       ]
     }
   ];
-
-  var input =     {
-    name: "MyMan",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-      2,
-      3,
-      1,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      3
-    ]
-  }
-
-function bestMatch(input){
-    var myResp = input.scores
-    var bestMatchScore = 0
-    var bestMatchName = ""
-    for(i=0; i < friends.length; i++){
-        var matchResp = friends[i].scores
-        var totalDiff = 0
-        for(x=0; x < matchResp.length; x++){
-            var diff = Math.abs(matchResp[x] - myResp[x])
-            totalDiff += diff
-        }
-        if(totalDiff > bestMatchScore){
-            bestMatchScore = totalDiff
-            bestMatchName = friends[i].name
-        }
-        console.log(totalDiff)
-    }
-    console.log(bestMatchScore + " " + bestMatchName)
-}
-
-bestMatch(input)
 
   module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,6 +5,29 @@
 
 var friends = require("../data/friends.js");
 
+function bestMatch(req){
+    var myResp = req.body.scores
+    var friendOptions = friends
+    var bestMatchScore = 100
+    var bestMatch = ""
+    for(i=0; i < friendOptions.length; i++){
+        var matchResp = friendOptions[i].scores
+        var totalDiff = 0
+        for(x=0; x < matchResp.length; x++){
+            var diff = Math.abs(matchResp[x] - myResp[x])
+            totalDiff += diff
+        }
+        if(totalDiff < bestMatchScore){
+            bestMatchScore = totalDiff
+            friendOptions[i].matchScore = bestMatchScore
+            bestMatch = friendOptions[i]
+        }
+        console.log(totalDiff)
+    }
+    friends.push(req.body);
+    return bestMatch
+}
+
 
 // ===============================================================================
 // ROUTING
@@ -12,14 +35,13 @@ var friends = require("../data/friends.js");
 
 module.exports = function(app) {
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
 
   app.post("/api/friends", function(req, res) {
-    // req.body is available since we're using the body parsing middleware
+    res.json(bestMatch(req));
     friends.push(req.body);
-    res.json(true);
     }
   )
 
@@ -29,3 +51,22 @@ module.exports = function(app) {
     res.json({ ok: true });
   });
 };
+
+// var myMan = {
+//     name: "MyMan",
+//     photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//     "scores":[
+//       2,
+//       3,
+//       1,
+//       4,
+//       5,
+//       1,
+//       2,
+//       5,
+//       4,
+//       3
+//     ]
+//   }
+
+//   bestMatch(myMan)
